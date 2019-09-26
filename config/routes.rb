38bfcd6d7914c95fac628c50d0e82d646f6876ba
masa95814 <masa95814@gmail.com>
2019-09-27 02:50:12 +0900

    投稿のルーティングをリソースベースに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
+  root to: 'posts#index'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
