bc4d95cf23cf01c84ffe3365127e579b084f0ef8
masa95814 <masa95814@gmail.com>
2019-11-03 02:13:23 +0900

    ホーム画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
   post "users/:id/update" => "users#update"
-  
   get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
   get "signup" => "users#new"
-  get "users/index"=> "users#index"
+  get "users/index" => "users#index"
   get "users/:id" => "users#show"
 
-  # 初期画面
-  root to: "posts#index"
-  
-  get "posts/index"
-  get "posts/show"
-  get "posts/new"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
-  get "posts/edit"
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+
+  # 初期画面
+  root to: "home#top"
+  get "about" => "home#about"
 end
