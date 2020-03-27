cc521d5146d9e947d6573458ee46336f2dab0664
saza <rorin1014@gmail.com>
2020-03-28 04:09:38 +0900

    メッセージ削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,6 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
-      flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
       @error_message = "メールアドレスまたはパスワードが間違っています"
