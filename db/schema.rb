da11849d31749f7b1d19d61edd95079d02b419e0
masa95814 <masa95814@gmail.com>
2020-03-27 23:36:04 +0900

    フォロー機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_162757) do
+ActiveRecord::Schema.define(version: 2020_03_27_135433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follow_relationships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_follow_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_follow_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_follow_relationships_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +39,16 @@ ActiveRecord::Schema.define(version: 2020_03_09_162757) do
     t.integer "user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -38,4 +58,8 @@ ActiveRecord::Schema.define(version: 2020_03_09_162757) do
     t.string "password"
   end
 
+  add_foreign_key "follow_relationships", "users"
+  add_foreign_key "follow_relationships", "users", column: "follow_id"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
